f736844ce994e2390ba5d92a9aa826bffc925f1a
ahmed amin <ahmed.amin@integrant.com>
2020-05-26 16:52:14 +0200

    Adding QueueUsingTwoStacks

---
+++ AlgPlayGroundApp/DataStructures/QueueUsingTwoStacks.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace AlgPlayGroundApp.DataStructures
+{
+    public class QueueUsingTwoStacks<T>
+    {
+        private Stack<T> stack1 = new Stack<T>();
+        private Stack<T> stack2 = new Stack<T>();
+
+        public bool IsEmpty => stack1.IsEmpty && stack2.IsEmpty;
+
+        public void Enqueue(T item)
+        {
+            stack1.Push(item);
+        }
+
+        public T Dequeue()
+        {
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException("Queue is empty");
+            }
+            MoveStack1ToStack2IfEmpty();
+
+            return stack2.Pop();
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException("Queue is empty");
+            }
+            MoveStack1ToStack2IfEmpty();
+
+            return stack2.Peek();
+        }
+
+        private void MoveStack1ToStack2IfEmpty()
+        {
+            if (stack2.IsEmpty)
+            {
+                while (!stack1.IsEmpty)
+                {
+                    stack2.Push(stack1.Pop());
+                }
+            }
+        }
+
+    }
+}
