116ad919ef6e6f20b851b103dda41e80ff545f65
ahmed amin <ahmed.amin@integrant.com>
2020-11-24 07:59:28 +0200

    adding PriorityQueueWithMaxHeap

---
+++ AlgPlayGroundApp/DataStructures/PriorityQueueWithMaxHeap.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace AlgPlayGroundApp.DataStructures
+{
+    /// <summary>
+    /// assuming the higher value has higher priority
+    /// </summary>
+    public class PriorityQueueWithMaxHeap<T> where T : IComparable<T>
+    {
+        private readonly Heap<T> _heap;
+        private const int DefaultCapacity = 5;
+        public PriorityQueueWithMaxHeap() : this(DefaultCapacity)
+        {
+           
+        }
+
+        public PriorityQueueWithMaxHeap(int capacity)
+        {
+            if (capacity < 1)
+            {
+                throw new ArgumentException("capacity should be greater than zero", nameof(capacity));
+            }
+            _heap = new Heap<T>(capacity);
+        }
+
+        public void Enqueue(T item)
+        {
+            _heap.Insert(item);
+        }
+
+        public T Dequeue()
+        {
+           return _heap.Remove();
+        }
+
+        public bool IsEmpty => _heap.IsEmpty;
+        public bool IsFull => _heap.IsFull;
+
+    }
+}
