d7eecd06a35a0fdec60b9c0873292fc449c45133
ahmed amin <ahmed.amin@integrant.com>
2021-01-22 06:28:04 +0200

    adding my solution to https://leetcode.com/problems/verifying-an-alien-dictionary/

---
+++ AlgPlayGroundApp/LeetCode/Easy/VerifyingAlienDict.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgPlayGroundApp.LeetCode.Easy
+{
+    /// <summary>
+    /// Problem link @ https://leetcode.com/problems/verifying-an-alien-dictionary/
+    /// </summary>
+    public class VerifyingAlienDict
+    {
+        public bool IsAlienSorted(string[] words, string order)
+        {
+            if(string.IsNullOrEmpty(order))
+                throw new ArgumentException(nameof(order));
+
+            if (words == null || !words.Any())
+                return true;
+
+            Dictionary<char,int> dict = new Dictionary<char, int>();
+            //adding blank character
+            var blank = ' ';
+            dict.Add(blank, -1);
+            var index = 0;
+            foreach (var ch in order)
+            {
+                dict.Add(ch,index++);
+            }
+
+            for (int i = 0; i < words.Length-1; i++)
+            {
+                if (!IsWordsSorted(words[i], words[i + 1]))
+                    return false;
+            }
+
+            return true;
+
+            bool IsWordsSorted(string w1, string w2)
+            {
+                if (w2.Length < w1.Length)
+                    w2 = w2.PadRight(w1.Length, blank);
+
+                for (int i = 0; i < w1.Length; i++)
+                {
+                    if (dict[w1[i]] < dict[w2[i]])
+                        return true;
+                    else
+                    {
+                        if ((dict[w1[i]] > dict[w2[i]]))
+                            return false;
+                    }
+                }
+
+                //both words have same characters & length
+                return true;
+            }
+        }
+    }
+}
---
+++ AlgPlayground.Tests/LeetCode/Easy/VerifyingAlienDictTests.cs
@@ -0,0 +1,45 @@
+﻿using AlgPlayGroundApp.LeetCode.Easy;
+using NUnit.Framework;
+
+namespace AlgPlayground.Tests.LeetCode.Easy
+{
+    public class VerifyingAlienDictTests
+    {
+
+        [Test]
+        public void TestInputReturnTrue()
+        {
+            var order = "hlabcdefgijkmnopqrstuvwxyz";
+            var words = new string[] {"hello", "leetcode"};
+            var cls = new VerifyingAlienDict();
+            var result = cls.IsAlienSorted(words, order);
+            Assert.AreEqual(true,result);
+
+            words = new string[] {"hello", "hello"};
+            order = "abcdefghijklmnopqrstuvwxyz";
+            result = cls.IsAlienSorted(words, order);
+            Assert.AreEqual(true, result);
+        }
+       
+
+        [Test]
+        public void TestInputReturnFalse()
+        {
+            var order = "abcdefghijklmnopqrstuvwxyz";
+            var words = new string[] { "apple", "app" };
+            var cls = new VerifyingAlienDict();
+            var result = cls.IsAlienSorted(words, order);
+            Assert.AreEqual(false, result);
+
+            order = "worldabcefghijkmnpqstuvxyz";
+            words = new string[] { "word", "world", "row" };
+            result = cls.IsAlienSorted(words, order);
+            Assert.AreEqual(false, result);
+
+            order = "loxbzapnmstkhijfcuqdewyvrg";
+            words = new string[] { "iekm", "tpnhnbe" };
+            result = cls.IsAlienSorted(words, order);
+            Assert.AreEqual(false, result);
+        }
+    }
+}
