a1b1838745702f49c4cf003af24afdaea492ad1a
ahmed amin <ahmed.amin@integrant.com>
2021-01-29 20:56:33 +0200

    MySolution to https://leetcode.com/problems/min-stack/solution/

---
+++ AlgPlayGroundApp/LeetCode/Easy/MinStack.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+
+namespace AlgPlayGroundApp.LeetCode.Easy
+{
+    /// <summary>
+    /// Problem : https://leetcode.com/problems/min-stack/solution/
+    /// </summary>
+    public class MySolution
+    {
+        public class MinStack
+        {
+            private const int DefaultSize = 5;
+            private int?[] _data;
+            public int TopIndex { get; private set; }
+            private int? _Min = null;
+
+            public MinStack()
+            {
+                _data = new int?[DefaultSize];
+                TopIndex = -1;
+            }
+
+            public void Push(int x)
+            {
+                if (TopIndex == _data.Length - 1)
+                {
+                    ExpandArray();
+                }
+
+                _data[++TopIndex] = x;
+                CalculateMin(0, TopIndex + 1);
+            }
+
+            private void ExpandArray()
+            {
+                var newSize = _data.Length + DefaultSize;
+                var newArray = new int?[newSize];
+                Array.Copy(_data, 0, newArray, 0, _data.Length);
+                _data = newArray;
+            }
+
+            private void CalculateMin(int startIndex, int length)
+            {
+                if (TopIndex < 0)
+                {
+                    _Min = null;
+                    return;
+                }
+
+                var validRange = new ArraySegment<int?>(_data, startIndex, length);
+                if (validRange.Count(i => i.HasValue) == 0)
+                {
+                    _Min = null;
+                    return;
+                }
+
+                _Min = validRange.Where(i => i.HasValue).Min();
+            }
+
+            public void Pop()
+            {
+                if (TopIndex < 0)
+                    return;
+
+                TopIndex--;
+                CalculateMin(0, TopIndex + 1);
+
+            }
+
+            public int Top()
+            {
+                return _data[TopIndex].GetValueOrDefault();
+            }
+
+            public int GetMin()
+            {
+                return _Min.GetValueOrDefault();
+            }
+        }
+    }
+}
---
+++ AlgPlayground.Tests/LeetCode/Easy/MinStackTests.cs
@@ -0,0 +1,24 @@
+﻿using AlgPlayGroundApp.LeetCode.Easy;
+using NUnit.Framework;
+
+namespace AlgPlayground.Tests.LeetCode.Easy
+{
+    public class MinStackTests
+    {
+        [Test]
+        public void FirstTest()
+        {
+            var minStack = new MySolution.MinStack();
+            minStack.Push(-2);
+            minStack.Push(0);
+            minStack.Push(-3);
+            var actual = minStack.GetMin(); // return -3
+            Assert.AreEqual(actual, -3);
+            minStack.Pop();
+            actual = minStack.Top();    // return 0
+            Assert.AreEqual(actual, 0);
+            actual = minStack.GetMin(); // return -2
+            Assert.AreEqual(actual, -2);
+        }
+    }
+}
